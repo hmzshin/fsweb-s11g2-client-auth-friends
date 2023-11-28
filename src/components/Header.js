6fcb334cc84ffaa75fe813016037ddcf9c027938
hmzshin <hamzasahin@outlook.com>
2023-11-28 23:57:00 +0300

    add stling to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">LOGIN.</Link>
-          </li>
-          <li>
-            <Link to="/friends">FRIENDLIST.</Link>
-          </li>
-          <li>
-            <Link to="/friends/add">ADDFRIEND.</Link>
-          </li>
-          <li>LOGOUT</li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header id="page-header">
+        <h1>FRIENDS DATABASE</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">LOGIN.</Link>
+            </li>
+            <li>
+              <Link to="/friends">FRIENDLIST.</Link>
+            </li>
+            <li>
+              <Link to="/friends/add">ADDFRIEND.</Link>
+            </li>
+            <li>
+              <p>LOGOUT</p>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <div className="horizontal-line"></div>
+    </>
   );
 };
 
