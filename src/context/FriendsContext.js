ad64c379905f6e7cc232cd0049f21c6f97f88ee4
hmzshin <hamzasahin@outlook.com>
2023-11-28 22:46:40 +0300

    add a constextapi to store friends data

---
+++ src/context/FriendsContext.js
@@ -0,0 +1,40 @@
+import { createContext, useReducer } from "react";
+
+export const FriendsContextObject = createContext();
+
+const friendReducer = (state, action) => {
+  switch (action.type) {
+    case "add_friend":
+      return [...state, action.payload];
+      break;
+
+    case "delete_friend":
+      return state.filter((s) => s.id !== action.payload);
+      break;
+
+    case "edit_friend":
+      return state.map((s) => {
+        if (s.id === action.payload.id) {
+          return action.payload;
+        }
+        return s;
+      });
+      break;
+
+    default:
+      return state;
+      break;
+  }
+};
+
+const FriendsContextProvider = ({ children }) => {
+  const [friends, dispatchFriends] = useReducer(friendReducer);
+
+  return (
+    <FriendsContextObject.Provider value={{ friends, dispatchFriends }}>
+      {children}
+    </FriendsContextObject.Provider>
+  );
+};
+
+export default FriendsContextProvider;
